bf7adaad7cc7c817f3a30ef61ddd878f7e60030a
bevannutakor <bevannutakor@icloud.com>
2023-06-06 12:38:52 -0700

    First commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+//using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+using PersonalFinanceManager.Models;
+
+
+namespace PersonalFinanceManager.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+        //options includes information to conncet to our database
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+}
+
---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+namespace PersonalFinanceManager.Models
+{
+	public class User
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		public string? email { get; set; }
+
+		[Required]
+		public string? password { get; set; }
+
+		[Required]
+		public string? name { get; set; }
+	}
+}
+
